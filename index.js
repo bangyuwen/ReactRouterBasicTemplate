e053624dc8f05bfd243798bb1e41e512ac39e6aa
BangYuWen <bangyuwen@gmail.com>
2016-07-13 16:19:42 +0800

    Ver1

---
+++ index.js
@@ -0,0 +1,14 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Hello = React.createClass({
+  render: function() {
+    return (
+      <div>
+        Hello
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(<Hello />, document.getElementById('content'));
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  entry: "./index.js",
+  output: {
+    filename: "./browser-bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel', // 'babel-loader' is also a legal name to reference
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+};
